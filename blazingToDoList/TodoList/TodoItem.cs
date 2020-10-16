ff6b0d074b55e58ee8303be7ca0fe5ed79fa63b8
Karen Chu <karen@Darrens-MBP.attlocal.net>
2020-10-15 22:18:37 -0700

    initializing to do list

---
+++ blazingToDoList/TodoList/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace blazingToDoList.TodoList
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
